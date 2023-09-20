c98bcaa6975295a712c6c287ee109eda64a51312
jroquebento <jroquebento@gmail.com>
2023-09-20 12:02:59 -0300

    toString de Pilha e classe Main

--- pilha/src/main/java/one/digitalinnovation/Pilha.java
+++ pilha/src/main/java/one/digitalinnovation/Pilha.java
@@ -34,5 +34,25 @@ public class Pilha {
         return false;*/
         return refNoEntradaPilha == null ? true : false;
     }
-    
+
+
+    @Override
+    public String toString() {
+        String stringRetorno = "-------------------\n";
+        stringRetorno += "    Pilha\n";
+        stringRetorno += "-------------------\n";
+
+        No noAuxiliar = refNoEntradaPilha;
+
+        while(true){
+            if(noAuxiliar != null){
+                stringRetorno += "[No{dado = " + noAuxiliar.getDado() + "}]\n";
+                noAuxiliar = noAuxiliar.getRefNo();
+            }else{
+                break;
+            }
+        }
+        stringRetorno += "===================\n";
+        return stringRetorno;
+    }
 }
