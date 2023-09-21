a8b21cf6aec5dc26b07a1390182ea936a54d256e
jroquebento <jroquebento@gmail.com>
2023-09-21 17:14:03 -0300

    toString de ListaEncadeada

--- lista-encadeada/src/main/java/one/digitalinnovation/ListaEncadeada.java
+++ lista-encadeada/src/main/java/one/digitalinnovation/ListaEncadeada.java
@@ -81,4 +81,16 @@ public class ListaEncadeada<T> {
     public boolean isEmpty(){
         return referenciaEntrada == null ? true : false;
     }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        No<T> noAuxiliar = referenciaEntrada;
+        for(int i = 0; i < this.size(); i++){
+            strRetorno +=  "[No{conteudo=" + noAuxiliar.getConteudo() + "}]--->";
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        strRetorno += "null";
+        return  strRetorno;
+    }
 }
