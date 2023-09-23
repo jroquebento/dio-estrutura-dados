0a48ed0dba3a336cb1fb5452cad4129b471392a0
jroquebento <jroquebento@gmail.com>
2023-09-23 10:06:09 -0300

    adicionando método add()

--- lista-circular/src/main/java/one/digitalinnovation/ListaCircular.java
+++ lista-circular/src/main/java/one/digitalinnovation/ListaCircular.java
@@ -1,9 +1,31 @@
 package one.digitalinnovation;
 
 public class ListaCircular<T> {
+    
     private No<T> cabeca;
     private No<T> cauda;
     private int tamanhoLista;
+
+    public ListaCircular() {
+        this.cauda = null;
+        this.cabeca = null;
+        this.tamanhoLista = 0;
+    }
+
+    public void add(T conteudo){
+        No<T> novoNo = new No<>(conteudo);    
+        if(this.tamanhoLista == 0) {
+            this.cabeca = novoNo;
+            this.cauda = this.cabeca;
+            this.cabeca.setNoProximo(cauda);
+        }
+        else{
+            novoNo.setNoProximo(this.cauda);
+            this.cabeca.setNoProximo(novoNo);
+            this.cauda = novoNo;
+        }
+        this.tamanhoLista++;
+    }
 
     public void remove(int index){
         if(index >= this.tamanhoLista){
