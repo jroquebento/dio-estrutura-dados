6e775462429009db5506f83fd347de8fa1b40269
jroquebento <jroquebento@gmail.com>
2023-09-26 11:12:55 -0300

    mudando println para print

--- arvore-binaria/src/main/java/one/digitalinnovation/ArvoreBinaria.java
+++ arvore-binaria/src/main/java/one/digitalinnovation/ArvoreBinaria.java
@@ -34,7 +34,7 @@ public class ArvoreBinaria<T extends Comparable<T>> {
     private void exibirInOrdem(BinNo<T> atual){
         if(atual != null){
             exibirInOrdem(atual.getNoEsq());
-            System.out.println(atual.getConteudo() + ", ");
+            System.out.print(atual.getConteudo() + ", ");
             exibirInOrdem(atual.getNoDir());
         }
     }
@@ -48,7 +48,7 @@ public class ArvoreBinaria<T extends Comparable<T>> {
         if(atual != null){
             exibirPosOrdem(atual.getNoEsq());
             exibirPosOrdem(atual.getNoDir());
-            System.out.println(atual.getConteudo() + ", ");
+            System.out.print(atual.getConteudo() + ", ");
         }
     }
 
@@ -59,7 +59,7 @@ public class ArvoreBinaria<T extends Comparable<T>> {
 
     private void exibirPreOrdem(BinNo<T> atual){
         if(atual != null){
-            System.out.println(atual.getConteudo() + ", ");
+            System.out.print(atual.getConteudo() + ", ");
             exibirPreOrdem(atual.getNoEsq());
             exibirPreOrdem(atual.getNoDir());
         }
