6b5817891dd696b63eecb83363bcee14f33bc2cc
jroquebento <jroquebento@gmail.com>
2023-09-26 14:42:52 -0300

    classe Carro, equals() e hashCode()

---
+++ equals-hash-code/src/main/java/one/digitalinnovation/Carro.java
@@ -0,0 +1,41 @@
+package one.digitalinnovation;
+
+import java.util.Objects;
+
+public class Carro {
+    String marca;
+
+    public Carro(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Carro other = (Carro) obj;
+        if (marca == null) {
+            if (other.marca != null)
+                return false;
+        } else if (!marca.equals(other.marca))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(marca);
+    }
+}
